af05252726016d03880c23e7cf1a35d5744f6558
Tony Suarez <suarezcumare@gmail.com>
2017-06-21 13:09:13 -0400

    second commit

--- money_bot.rb
+++ money_bot.rb
@@ -14,11 +14,11 @@ Telegram::Bot::Client.run(token) do |bot|
       bot.api.send_message(chat_id: message.chat.id, text: "Bot para saber el precio del dolar, la tasa de cambios y generales que quiera hacer")
     
     when '/moneycl'
-      response = HTTParty.get('http://mindicador.cl/api')
-      bot.api.send_message(chat_id: message.chat.id, text: "#{response.to_s}")
+      #response = HTTParty.get('http://mindicador.cl/api')
+      #bot.api.send_message(chat_id: message.chat.id, text: "#{response.to_s}")
     when '/moneyvzla'
-      response = HTTParty.get('https://s3.amazonaws.com/dolartoday/data.json')
-      bot.api.send_message(chat_id: message.chat.id, text: "#{response.to_s}")
+      #response = HTTParty.get('https://s3.amazonaws.com/dolartoday/data.json')
+      #bot.api.send_message(chat_id: message.chat.id, text: "#{response.to_s}")
     end
   end
 end
